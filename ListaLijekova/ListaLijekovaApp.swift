d08e85367a2aee49268a6dbda6b8f32762d07896
AjdinAlic <ajdin.alic@saltsquare.io>
2022-10-19 09:32:20 +0200

    Initial Commit

---
+++ ListaLijekova/ListaLijekovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListaLijekovaApp.swift
+//  ListaLijekova
+//
+//  Created by Ajdin on 19. 10. 2022..
+//
+
+import SwiftUI
+
+@main
+struct ListaLijekovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
